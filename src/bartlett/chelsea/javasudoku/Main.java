9397cc9af53ffa94fba0965ebe0a89e5dc7ce5b4
Nicholas Bartlett <nbartlett84@gmail.com>
2014-02-19 20:59:34 -0500

    nick made his first initial change and commit

--- src/bartlett/chelsea/javasudoku/Main.java
+++ src/bartlett/chelsea/javasudoku/Main.java
@@ -2,7 +2,6 @@ package bartlett.chelsea.javasudoku;
 
 public class Main {
 	public static void main (String[] args){
-		System.out.println("Hello World");
+		System.out.println("Hello World.  This is going to be so much fun!!!");
 	}
-
 }
